b00c6a899ce9ee9a4022fb0daecbac2cff124fff
cecilestaller <c.staller96@gmail.com>
2023-10-26 13:02:17 +0200

    modulares fenster zur individuellen teamnamen bestimmung eingebaut

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,3 +17,17 @@ const resetButton = () => {
     scoreAway.innerText = 0;
     scoreHome.innerText = 0;
 }
+
+// ===== Bonus --> modales Fenster
+// ist ein fenster, was weitere user interaktionen blockt, bis es weggeklickt wird.
+
+let homeTeam = document.body.querySelector('#home-team');
+let awayTeam = document.body.querySelector('#away-team');
+
+const setTeams = () => {
+    const homeTeamName = window.prompt("Gib einen Teamnamen für Home ein");
+    const awayTeamName = window.prompt("Gib einen Teamnamen für Away ein");
+    homeTeam.textContent = homeTeamName;
+    awayTeam.textContent = awayTeamName;
+
+}
